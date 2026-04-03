4833e99a925faf9168332b7f1e7edb48b860a482
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-04-03 11:16:24 +0530

    allow to request from postman and frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import transactionRouter from "./routes/transaction.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import adminRouter from "./routes/admin.route.js";
+import cors from "cors";
 /**
  * @module  App
  * @description The core Express application instance. 
@@ -19,7 +20,12 @@ const app= express();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+    origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL : true, // 👈 Ya toh frontend ka link ya sabke liye "*"
+    credentials: true,
+   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use("/api/auth",authRouter);
 app.use("/api/transactions",transactionRouter);
 app.use("/api/admin",adminRouter); // Admin routes
