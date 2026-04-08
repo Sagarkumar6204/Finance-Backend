cb6510f39cbf5d56627346c86c68a12576432c85
Sagar Kumar <bittukumar9006716@gmail.com>
2026-04-08 23:44:10 +0530

    Change server listen address to '0.0.0.0'

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,6 @@ connectDB();
  * @description The port on which the server will listen for incoming HTTP requests.
  */
 const PORT=process.env.PORT || 3000;
-app.listen(PORT,()=>{
+app.listen(PORT,'0.0.0.0',()=>{
     console.log("server is running on PORT: ",PORT);
 })
